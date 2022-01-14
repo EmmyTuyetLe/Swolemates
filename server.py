4f03ca2006077cd988595157de9f48ba78292627
Emmy Le <emilytuyetle@gmail.com>
2022-01-14 14:49:49 -0800

    finished routes

--- server.py
+++ server.py
@@ -18,51 +18,61 @@ app.jinja_env.undefined = StrictUndefined
 @app.route("/")
 def homepage():
     """View homepage."""
-
     return render_template("homepage.html")
 
+@app.route("/user_home")
+def main():
+
+    if session.get("user_id"):
+        user = crud.get_user_by_email(session.get("user_email"))   
+        return render_template("user_home.html", user=user, MY_API_KEY =MY_API_KEY)
+
+    else:
+        return render_template("homepage.html", MY_API_KEY = MY_API_KEY)
+
+@app.route("/login")
+def show_login():
+    """Show login form."""
+
+    return render_template("login.html")
+
 @app.route("/login", methods=["POST"])
-def login():
+def login_user():
     """Login existing users"""
     email = request.form.get("email")
     password = request.form.get("password")
     user = crud.get_user_by_email(email)
+    print(user)
     if user:
         if user.email == email and user.password == password:
+            print(user.email, email)
+            session["user_id"] = user.user_id
             session["user_email"] = user.email
+            session["fname"] = user.fname
             flash(f"Welcome back, {user.email}!")
-            return redirect("/")
-    else:
-        flash("Incorrect password or email. Please try again")
+            return redirect("/user_home")
+        else:
+            flash("Incorrect password or email. Please try again")
     return redirect("/login")
 
-@app.route("/login_button")
-def login_page():
-    """Tell user if they are logged in or not"""
-    if session.get("user_id"):
-        flash("You're already logged in")
-    else:
-        new_session = session.get("user_id")   
-        return render_template("login.html", new_session=new_session)
-    return redirect("/")
 
 @app.route("/logout")
 def logout():
-    """Logout users in session"""
-    if session.get("user_id"):
-        session.clear()
-        flash("successfully logged out.")
-    else:
-        flash("Already logged out")
-    return redirect("/")
+    """Logout User and End Session"""
+    if "user_id" in session:
+        del session["user_id"]
+        flash("Logged out. Thanks for visiting.")
+        return render_template("logout.html")
+    else: 
+        return redirect("/")
 
-@app.route("/user")
+@app.route("/my_profile")
 def to_user_profile():
     """For user to view their profile"""
-    if session.get("user_id"):
-        return render_template("user.html")
+    if "user_id" in session:
+        return render_template("user_home.html")
     else:
-        flash("Login to see your user page")
+        flash("You must be logged in to see your profile.")
         return redirect("/")
 
 
@@ -80,14 +90,16 @@ def create_user():
     """Create new user account"""
     email = request.form.get("email")
     password = request.form.get("password")
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
     user = crud.get_user_by_email(email)
     if user:
         flash("That email is already associated with an account.")
     else:
-        crud.create_user(email, password)
+        crud.create_user(email, password, fname, lname)
         flash("Account created! Please log in.")
     
-    return redirect("/login_button")
+    return redirect("/login")
 
 
 if __name__ == "__main__":
