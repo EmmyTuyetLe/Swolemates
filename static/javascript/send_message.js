afb548c5cbf074420bc0ed75e7b3e538af2b0669
Emmy Le <emilytuyetle@gmail.com>
2022-01-24 17:29:07 -0800

    fixed send messages

--- static/javascript/send_message.js
+++ static/javascript/send_message.js
@@ -1,26 +1,36 @@
 'use strict';
 
-document.querySelector("#send_message_form").addEventListener("submit", evt => {
-  evt.preventDefault();
-  console.log("Clicked!!!!!!*******");
+let forms = document.querySelectorAll(".send_message_form")
+console.log(forms)
 
-  const formInputs = {
-    buddy_id: document.querySelector("#buddy_id").value,
-    user_id: document.querySelector("#sender_id").value,
-    message_content: document.querySelector("#message_text").value
-  };
+for (let form of forms){
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
 
-  console.log(formInputs);
+    let buddyId = evt.target.id.split("_")[2]
+    console.log(buddyId);
+  
+    const formInputs = {
+      buddy_id: document.querySelector(`#buddy_id_${buddyId}`).value,
+      user_id: document.querySelector("#unsaver_id").value,
+      message_content: document.querySelector("#message_text").value
+    };
 
-  fetch("/send_message.json", {
-    method: "POST",
-    body: JSON.stringify(formInputs),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(response => response.json())
-    .then(responseJson => {
-      alert(responseJson.status);
-    });
-});
+    console.log(formInputs)
+    fetch("/send_message.json", {
+      method: "POST",
+      body: JSON.stringify(formInputs),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
+        alert(responseJson.status);
+        
+      });
+  });
+}
+
+
